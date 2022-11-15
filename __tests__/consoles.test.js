b138cd808d8f0ede8d4144230e77dff71bddc778
Tanner Richards <Tanner.richards990@gmail.com>
2022-11-14 22:28:24 -0800

    test passing for getting all the consoles

---
+++ __tests__/consoles.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('#GET /consoles should return all consoles', async () => {
+    const response = await request (app).get('/consoles');
+    expect(response.status).toBe(200);
+    expect(response.body.length).toEqual(3);
+    expect(response.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      released: expect.any(String),
+      
+    });
+  });
+}); 
